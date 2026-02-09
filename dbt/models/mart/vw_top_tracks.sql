8f2c8aef1b276ffb6040b8f6dde6b124f4e2c0fc
BRYAN ALEJANDRO QUEZADA VASQUEZ <bryan_3009@hotmail.com>
2026-02-09 07:04:50 +0000

    upgrade top tracks metrics

--- dbt/models/mart/vw_top_tracks.sql
+++ dbt/models/mart/vw_top_tracks.sql
@@ -13,10 +13,18 @@ SELECT
     artist_name,
     album_name,
     track_name,
-    COUNT(*) AS play_count_2025,
-    DATE(MIN(first_ever_played)) AS first_ever_played,  -- First time EVER
-    DATE(MIN(listening_timestamp)) AS first_played_date,
-    DATE(MAX(listening_timestamp)) AS last_played_date,
+    COUNT(*) AS play_count,
+    DATE(MIN(first_ever_played)) AS first_ever_played,
+    
+    -- Time metrics
+    ROUND(SUM(ms_played) / 60000.0, 2) AS total_minutes,
+    ROUND(SUM(ms_played) / 3600000.0, 2) AS total_hours,
+    ROUND(AVG(ms_played) / 60000.0, 2) AS avg_minutes_per_play,
+    ROUND(AVG(ms_played) / 1000.0, 2) AS avg_seconds_per_play,
+    
+    -- Dates
+    DATE(MIN(listening_timestamp)) AS first_played_date_2025,
+    DATE(MAX(listening_timestamp)) AS last_played_date_2025,
     (SELECT DATE(MAX(listening_timestamp)) 
      FROM streaming_history
      WHERE user_id = 0001) AS latest_snapshot_date
@@ -27,4 +35,4 @@ GROUP BY
     artist_name,
     album_name,
     track_name
-ORDER BY play_count_2025 DESC
+ORDER BY play_count DESC

bec12852e310296abd15c92e2ab93ec5054f9d97
BRYAN ALEJANDRO QUEZADA VASQUEZ <bryan_3009@hotmail.com>
2026-02-09 05:54:21 +0000

    initial fact structure

--- dbt/models/dim/dim_countries.sql
+++ dbt/models/dim/dim_countries.sql
@@ -1,13 +1,21 @@
-
-
 WITH country_data AS (
     SELECT DISTINCT
         country
+        ,MAX(meta_last_modified) AS meta_last_modified
     FROM {{ ref('stg_listening') }}
     WHERE country IS NOT NULL
+    GROUP BY country
 )
 
-SELECT 
-    {{ dbt_utils.generate_surrogate_key(['country']) }}  AS country_id
-    ,country
-FROM country_data
+,country_detailed AS (
+    SELECT * FROM {{ ref('seed_countries') }}
+)
+
+SELECT
+    {{ dbt_utils.generate_surrogate_key(['CD.country']) }}  AS country_id
+    ,CDL.country
+    ,CDL.name
+    ,CD.meta_last_modified
+
+FROM country_data CD
+LEFT JOIN country_detailed CDL ON CD.country = CDL.country
